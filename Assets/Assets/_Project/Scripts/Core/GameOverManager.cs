35025fa7f443595056d565003040f3d204db0f1d
Michal Lazovy <m.lazovy@rgu.ac.uk>
2025-12-05 22:22:14 +0000

    Remove unused GameOverManager class methods and clean up imports

--- Assets/Assets/_Project/Scripts/Core/GameOverManager.cs
+++ Assets/Assets/_Project/Scripts/Core/GameOverManager.cs
@@ -1,16 +1,3 @@
 using UnityEngine;
-
-public class GameOverManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+using TMPro;
+using UnityEngine.SceneManagement; // Needed to reload level
