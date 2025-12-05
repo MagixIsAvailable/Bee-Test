00db842dd7f7daac2f5fb4dfe10c096463d2c33e
Michal Lazovy <m.lazovy@rgu.ac.uk>
2025-12-05 18:58:10 +0000

    Refactor SimpleFlower class: update resource handling and improve audio effect management

--- Assets/Assets/_Project/Scripts/Core/SimpleFlower.cs
+++ Assets/Assets/_Project/Scripts/Core/SimpleFlower.cs
@@ -2,37 +2,43 @@ using UnityEngine;
 
 public class SimpleFlower : MonoBehaviour
 {
-    public GameObject pollenFxPrefab;
-    public AudioClip collectSound; // Drag your Chime here!
+    [Header("Visuals & Audio")]
+    public GameObject collectFxPrefab; // Pollen=Yellow, Water=Blue
+    public AudioClip collectSound;     // Chime=Flower, Splash=Water
+
+    [Header("Resource Values")]
+    // Set these in the Inspector for each type!
+    public int pollenValue = 1;        // Score
+    public float nectarValue = 20f;    // Stamina
+    public int waterValue = 0;         // Water Count (New!)
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             // 1. Play Sound
-            // We get the AudioSource from the Player
-            AudioSource[] sources = other.GetComponents<AudioSource>();
+            if (collectSound != null) AudioSource.PlayClipAtPoint(collectSound, transform.position);
 
-            // We grab the SECOND AudioSource (Index 1) because Index 0 is the buzzing
-            // Or we just use PlayClipAtPoint to be safe
-            if (collectSound != null)
-            {
-                // Plays sound at the flower's location
-                AudioSource.PlayClipAtPoint(collectSound, transform.position);
-            }
+            // 2. Spawn FX
+            if (collectFxPrefab != null) Instantiate(collectFxPrefab, transform.position, Quaternion.identity);
 
-            // 2. FX
-            if (pollenFxPrefab != null)
-            {
-                Instantiate(pollenFxPrefab, transform.position, Quaternion.identity);
-            }
-
-            // 3. Logic
+            // 3. Add Resources (Only if value > 0)
             if (GameManager.Instance != null)
             {
-                GameManager.Instance.AddPollen(1);
+                if (pollenValue > 0) GameManager.Instance.AddPollen(pollenValue);
+                if (waterValue > 0) GameManager.Instance.AddWater(waterValue); // You need to add AddWater to Manager
             }
 
+            // 4. Refill Stamina
+            if (nectarValue > 0)
+            {
+                BeeController bee = other.GetComponent<BeeController>();
+                if (bee != null) bee.RestoreStamina(nectarValue);
+            }
+
+            // 5. Destroy/Hide
+            // If it's a flower, hide it. If it's a pond, maybe don't hide it?
+            // For now, let's disable it so you can't collect it infinite times instantly.
             gameObject.SetActive(false);
         }
     }
